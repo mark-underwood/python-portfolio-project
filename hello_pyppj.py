67b93213270913d6f3fdfb32ebd3b9ec67c865b6
Mark Underwood <mark.thunderwood@gmail.com>
2024-11-12 19:58:35 -0600

    0.0.3 lots

--- hello_pyppj.py
+++ hello_pyppj.py
@@ -7,66 +7,107 @@
 
 class Actor(): # superclass
     """ generic actor template """
-    def __init__(self, target, hp, hp_max, ap, ap_max, mp, mp_max, sp, sp_max):
+    def __init__(self, name, hp_max, ap_max, mp_max, sp_max):
         self.target = None
-        self.hp = hp # health
+        self.name = name
+        self.hp = hp_max # health
         self.hp_max = hp_max
-        self.ap = ap # action
+        self.ap = ap_max # action
         self.ap_max = ap_max
-        self.mp = mp # mana
+        self.mp = mp_max # mana
         self.mp_max = mp_max
-        self.sp = sp # stamina
+        self.sp = sp_max # stamina
         self.sp_max = sp_max
-        damage_dealt = 0
+        self.damage_dealt = 0
 
     def set_target(self, target):
         """ set focus target """
         # need target and datatype validation
         self.target = target # WIP
+        print("Set target:", target)
 
     def weapon_attack(self, target):
         """ attack target """
-        print("weapon_attack stub")
+        print("weapon_attack stub:", target)
 
     def use_item(self, target):
         """ use an item """
-        print("use_item stub")
+        print("use_item stub:", target)
 
 class Player(Actor): # subclass
     """ player character subclass """
-    def __init__(self, target, hp, hp_max, ap, ap_max, mp, mp_max, sp, sp_max):
-        super().__init__(target, hp, hp_max, ap, ap_max, mp, mp_max, sp, sp_max)
-        print("actor stub")
-        self.hp = hp_max # health
-        self.ap = ap_max # action
-        self.mp = mp_max # mana
-        self.sp = sp_max # stamina
-
+    def __init__(self, name, hp_max, ap_max, mp_max, sp_max):
+        super().__init__(name, hp_max, ap_max, mp_max, sp_max)
+        print(f"{name.capitalize()} has entered the game.")
 
 class NonPlayerCharacter(Actor): # subclass
     """ NPC subclass """
-    def __init__(self, target, hp, hp_max, ap, ap_max, mp, mp_max, sp, sp_max):
-        super().__init__(target, hp, hp_max, ap, ap_max, mp, mp_max, sp, sp_max)
-        print("npc stub")
+    def __init__(self, name, hp_max, ap_max, mp_max, sp_max):
+        super().__init__(name, hp_max, ap_max, mp_max, sp_max)
+        print(f"ENEMY SIGHTED: {name}")
+
+class Combat():
+    """ combat """
+    def __init__(self):
+        print('combat stub')
 
 class Game():
-    """ combat loop """
+    """ game """
     def __init__(self, difficulty):
         self.difficulty = difficulty
+        self.player = None
+        self.round_count = 0 # always start at zero
+        self.rounds_ref = ({
+            'easy': 1,
+            'normal': 3,
+            'hard': 5,
+            'nightmare': 9
+            },) # tuple + dict # a dictionary made read only by tuple encapsulation
+        self.round_limit = self.rounds_ref[0][self.difficulty]
+        # generate queue with initial node count = self.rounds_ref[0][difficulty]
 
     def start(self):
         """ begin running the game """
-        print(f"Do something? Difficulty is: {self.difficulty.upper()}") # stub
+        print(f"Game start. Difficulty is: {self.difficulty.upper()}") # stub
+        
+        self.player = Player('PLAYER',
+            100, # health
+            100, # action
+            100, # mana
+            100) # stamina
 
-        # define player as Player(arg)
+        print(f"A '{self.difficulty}' game should play",
+              f"for {self.rounds_ref[0][self.difficulty]} rounds.")
 
-        # while True:
-        #     ???
-        # return (game_state_goes_here?)
-
-        print("???\nProfit.") # joke stub
-
-        print("\n GAME OVER\n") # generic game over message
+        while True: # do rounds
+            self.round_count += 1 # increment round counter
+            print(f'STUB: Round {self.round_count} START')
+            print('StubA) pre-round story/RP')
+            print('StubB) combat')
+            print('StubC) post combat story/RP')
+            print(f'STUB: Round {self.round_count} OVER')
+            if self.round_count >= self.rounds_ref[0][self.difficulty]:
+                while True:
+                    print("\n You found a way to safety. Continue fighting?\n")
+                    choice = input("Type 'fight' or 'leave': ")
+                    if choice.lower() == 'fight':
+                        print('StubD) add another queue noded, increase rount_limit, and advance')
+                        self.round_limit += 1
+                        break
+                    if choice.lower() == 'leave':
+                        print('StubD) Chance at final boss fight here !?')
+                        break
+                    print("Please type only 'fight' or 'leave'.")
+            if self.round_count >= self.round_limit:
+                break
+        
+        # post rounds conclusion
+        if self.player.hp <= 0:
+            # bad endings here
+            print("\n GAME OVER\n") # generic game over message
+        else:
+            # good endings here
+            print(f"{self.player.name} met ol' Trusty the horse and rode off into the sunset.")
 
 def main():
     """ main """
@@ -74,12 +115,13 @@ def main():
     game_title = 'FANTASY GAUNTLET PythonPPJ'
     print("Welcome.\n")
     while True: # menu loops
-        print(f'\n {game_title} | MAIN MENU \n')
-        print("1) Start game on NORMAL\n"+
-              "2) Start game on HARD\n"+
-              "3) Start game on NIGHTMARE\n"+
-              "9) Quit\n")
-        choice = input("Select an option: ")
+        print(f'\n |\n | {game_title}\n-|===============================>'+
+        '\n | MAIN MENU\n |\n')
+        print("1) Start NORMAL\n"+
+              "2) Start HARD\n"+
+              "3) Start NIGHTMARE\n"+
+              "Q) Quit\n")
+        choice = input("Select an option (# or Q): ")
         # print(f"You chose {choice}")
         if choice == '1':
             current_game = Game('normal') # setup normal game
@@ -90,10 +132,10 @@ def main():
         elif choice == '3':
             current_game = Game('nightmare') # setup nightmare game
             current_game.start() # begin
-        elif choice == '9' or choice.lower == 'exit' or choice.lower == 'quit':
+        elif choice.lower() == 'exit' or choice.lower() == 'quit' or choice.lower() == 'q':
             print("Exiting ...")
             return
         else:
-            print(f"\n Choice '{choice if (len(choice) <= 32) else '???'}' is invalid.\n")
+            print(f"\n Choice '{choice if (len(choice) <= 32) else '???'}' is not available.\n")
 
 main()
