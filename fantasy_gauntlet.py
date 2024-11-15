006a34d7ab7626270bac0d9c5e7287100e5a0989
Mark Underwood <mark.thunderwood@gmail.com>
2024-11-15 17:26:29 -0600

    0.0.11 added actor_pkg ; tweaked press_enter

--- fantasy_gauntlet.py
+++ fantasy_gauntlet.py
@@ -1,5 +1,7 @@
 """ ppj """
 
+from actor_pkg.player import Player
+from actor_pkg.npc import NonPlayerCharacter
 from utils_pkg.press_enter import press_enter_to_continue
 
 # 1 need a queue for enemies to fight
@@ -7,62 +9,15 @@ from utils_pkg.press_enter import press_enter_to_continue
 # 3 enemies will be fought on a first come first serve basis
 # 4 defeated enemies may be added to a linked list and dequeued
 
-class Actor(): # superclass
-    """ generic actor template """
-    def __init__(self, name, stat):
-        # hp_max, ap_max, mp_max, sp_max):
-        self.target = None
-        self.name = name
-        self.stat = stat
-        self.stat['hp'] = stat['hp_max'] # health
-        self.stat['ap'] = stat['ap_max'] # action
-        self.stat['mp'] = stat['mp_max'] # mana
-        self.stat['sp'] = stat['sp_max'] # stamina
-        self.stat['damage_dealt'] = 0
-        self.stats_str = (
-            f"  HEALTH: {self.stat['hp']} / {self.stat['hp_max']}\n"
-            f"  ACTION: {self.stat['ap']} / {self.stat['ap_max']}\n"
-            f"    MANA: {self.stat['mp']} / {self.stat['mp_max']}\n"
-            f" STAMINA: {self.stat['sp']} / {self.stat['sp_max']}\n"
-        )
-
-    def set_target(self, target):
-        """ set focus target """
-        # need target and datatype validation
-        self.target = target # WIP
-        print("Set target:", target)
-
-    def weapon_attack(self, target):
-        """ attack target """
-        print("weapon_attack stub:", target)
-
-    def use_item(self, target):
-        """ use an item """
-        print("use_item stub:", target)
-
-    def show_stats(self):
-        """ show actor stats """
-        print(self.stats_str)
-
-    def __str__(self):
-        return self.stats_str
-
-class Player(Actor): # subclass
-    """ player character subclass """
-    def __init__(self, name, stat):
-        super().__init__(name, stat)
-        print(f"{name.capitalize()} has entered the game.")
-
-class NonPlayerCharacter(Actor): # subclass
-    """ NPC subclass """
-    def __init__(self, name, stat):
-        super().__init__(name, stat)
-        print(f"ENEMY SIGHTED: {name}")
-
 class Combat():
     """ combat """
     def __init__(self):
         print('combat stub')
+
+class RoundInstance():
+    """ role-playing time | stuff happens """
+    def __init__(self):
+        print('Enterng the arena role playing time')
 
 class Game():
     """ game """
@@ -70,6 +25,8 @@ class Game():
         self.difficulty = difficulty
         self.player = None
         self.round_count = 0 # always start at zero
+        self.current_round = None # round_instance object
+        self.round_instances = [] # saved round states
         self.rounds_ref = ({
             'easy': 1,
             'normal': 3,
@@ -96,6 +53,9 @@ class Game():
         while True: # do rounds
             self.round_count += 1 # increment round counter
             print(f'STUB: Round {self.round_count} START')
+            self.current_round = RoundInstance()
+            self.round_instances.append(self.current_round)
+            self.current_round = None
             print('StubA) pre-round story/RP')
             print('StubB) combat')
             print('StubC) post combat story/RP')
--- utils_pkg/press_enter.py
+++ utils_pkg/press_enter.py
@@ -5,5 +5,5 @@ from getpass import getpass # for hiding user input
 def press_enter_to_continue():
     """ silent pause before continuing """
     # pylint does not complain 'unused variable' if starts with underscore
-    _dump_this = getpass(" [ press enter to continue ]")
+    _dump_this = getpass(" [ press enter to continue ]\n")
     print('_____________________________')
