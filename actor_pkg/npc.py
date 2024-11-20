acd24fa3511504e16da7ad2a7163b54d72b4fd41
Mark Underwood <mark.thunderwood@gmail.com>
2024-11-20 17:53:38 -0600

    0.0.20 Actor, Player, NPC class changes. +methods +validation D.R.Y.

--- actor_pkg/npc.py
+++ actor_pkg/npc.py
@@ -1,12 +1,22 @@
 """ non player character subclass """
 
+import random
 from actor_pkg.actor import Actor
 
 class NonPlayerCharacter(Actor): # subclass
     """ NPC subclass """
-    def __init__(self, name, stat, is_player_faction = False):
+    def __init__(self, name, stat, is_player_faction = False): # enemy by default
         super().__init__(name, stat)
+        self.stat["is_player_faction"] = is_player_faction # override default
+        self.new_id()
+
         if is_player_faction: # enemy unless otherwise stated
-            print(f"An ally {name} has joined you!")
+            print(f"An ally {self.name} has joined you!")
         else:
-            print(f"ENEMY SIGHTED: {name}")
+            print(f"ENEMY SIGHTED: {self.name}")
+
+    def new_id(self):
+        """ generate actor id """
+        # run once, while loop ( check if unique -> run again ) # can break targeting
+
+        self.actor_id = random.randint(1000, 65535)
--- actor_pkg/player.py
+++ actor_pkg/player.py
@@ -1,9 +1,21 @@
 """ player character subclass """
 
+import random
 from actor_pkg.actor import Actor
 
 class Player(Actor): # subclass
     """ player character subclass """
     def __init__(self, name, stat):
         super().__init__(name, stat)
-        print("You stand up and see more light in one direction. ")
+        self.new_id()
+        if self.name.lower() == "you":
+            s = ''
+        else:
+            s = 's'
+        print(f"{self.name.capitalize()} stand{s} up and see{s} more light in one direction. ")
+
+    def new_id(self):
+        """ generate actor id """
+        # run once, while loop ( check if unique -> run again ) # can break targeting
+
+        self.actor_id = random.randint(0, 999)
